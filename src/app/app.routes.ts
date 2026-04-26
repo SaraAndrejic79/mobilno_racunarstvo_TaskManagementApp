c61dc017f4b1c117ee1dcd7d43dd0e56347ac102
SaraAndrejic <saraandrejic2468@gmail.com>
2026-04-26 13:28:02 +0200

    Dodat auth guard kod svih ruta sem login/register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,40 +1,49 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   {
     path: 'register',
-    loadComponent: () => import('./pages/register/register.page').then( m => m.RegisterPage)
+    loadComponent: () => import('./pages/register/register.page').then(m => m.RegisterPage)
   },
   {
     path: 'login',
-    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+    loadComponent: () => import('./pages/login/login.page').then(m => m.LoginPage)
   },
   {
     path: 'dashboard',
-    loadComponent: () => import('./pages/dashboard/dashboard.page').then( m => m.DashboardPage)
+    loadComponent: () => import('./pages/dashboard/dashboard.page').then(m => m.DashboardPage),
+    canActivate: [authGuard]
   },
   {
     path: 'add-task',
-    loadComponent: () => import('./pages/add-task/add-task.page').then( m => m.AddTaskPage)
+    loadComponent: () => import('./pages/add-task/add-task.page').then(m => m.AddTaskPage),
+    canActivate: [authGuard]
   },
-  { path: 'add-task/:id',
-     loadComponent: () => import('./pages/add-task/add-task.page').then(m => m.AddTaskPage) },
-
+  {
+    path: 'add-task/:id',
+    loadComponent: () => import('./pages/add-task/add-task.page').then(m => m.AddTaskPage),
+    canActivate: [authGuard]
+  },
   {
     path: 'tasks',
-    loadComponent: () => import('./pages/tasks/tasks.page').then( m => m.TasksPage)
+    loadComponent: () => import('./pages/tasks/tasks.page').then(m => m.TasksPage),
+    canActivate: [authGuard]
   },
   {
     path: 'stats',
-    loadComponent: () => import('./pages/stats/stats.page').then( m => m.StatsPage)
+    loadComponent: () => import('./pages/stats/stats.page').then(m => m.StatsPage),
+    canActivate: [authGuard]
   },
   {
     path: 'profile',
-    loadComponent: () => import('./pages/profile/profile.page').then( m => m.ProfilePage)
+    loadComponent: () => import('./pages/profile/profile.page').then(m => m.ProfilePage),
+    canActivate: [authGuard]
   },
   {
     path: 'calendar',
-    loadComponent: () => import('./pages/calendar/calendar.page').then( m => m.CalendarPage)
+    loadComponent: () => import('./pages/calendar/calendar.page').then(m => m.CalendarPage),
+    canActivate: [authGuard]
   },
 ];
